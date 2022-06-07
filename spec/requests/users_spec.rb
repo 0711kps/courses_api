1ab6613e94ee8d5dd8b38021d3b680247680d5de
🫠🇵 🇪 🇷 🛟 <0711kps@gmail.com>
2022-06-07 15:49:31 +0800

    Test: spec for admin auth in users#

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,12 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
+  let(:header_with_token) do
+    {
+      'Authorization' => 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoid29vbCIsImV4cGlyZWRfYXQiOiIyMDIyLTA2LTA3IDE2OjIwOjQ3ICswODAwIn0.KPokejZb4V5DvvDN_KB4q8ZSOa5xIgBbSMcl69Xe8a4'
+    }
+  end
   describe "POST /users" do
     let(:path) { '/users' }
+    
+    context 'When legal token is not contained in header' do
+      let(:sending_data) { { email: 'user@correct.mail.com', name: 'username' } }
+      it 'Response with 401' do
+        post path, params: sending_data
+        expect(response).to have_http_status(401)
+      end
+    end
+    
     context 'When email not match regexp' do
       let(:sending_data) { { email: 'wrong-params' } }
       it 'Response with 400' do
-        post path, params: sending_data
+        post path, params: sending_data, headers: header_with_token
         expect(response).to have_http_status(400)
       end
     end
@@ -14,7 +28,7 @@ RSpec.describe "Users", type: :request do
     context 'When email match regexp' do
       let(:sending_data) { { email: 'user@correct.mail.com', name: 'username' } }
       it 'Response with 201 and user created' do
-        post path, params: sending_data
+        post path, params: sending_data, headers: header_with_token
         expect(response).to have_http_status(201)
         expect(Data.users[:id]).to eq(1)
         expect(Data.users[:data]).to eq(
@@ -128,6 +142,13 @@ RSpec.describe "Users", type: :request do
   describe 'PUT /users/1' do
     let(:path) { '/users/1' }
 
+    context 'When legal token is not contained in header' do
+      it 'Response with 401' do
+        put path
+        expect(response).to have_http_status(401)
+      end
+    end
+
     context 'When user exist' do
       let(:user_data) do
         {
@@ -144,7 +165,7 @@ RSpec.describe "Users", type: :request do
 
       it 'Response with 200 and updated data' do
         Data.users[:data] << user_data.merge(id: 1)
-        put path, params: updated_user_data
+        put path, params: updated_user_data, headers: header_with_token
         expect(response).to have_http_status(200)
         expect(Data.users[:data]).to eq([{id: 1, name: 'user-x', email: 'user-x@mail.com'}])
         expect(JSON.parse(response.body)['data'].symbolize_keys).to eq(updated_user_data.merge(id: 1))
@@ -153,7 +174,7 @@ RSpec.describe "Users", type: :request do
 
     context 'When user not exist' do
       it 'Response with 400' do
-        put path
+        put path, headers: header_with_token
         expect(response).to have_http_status(400)
       end
     end
@@ -162,6 +183,13 @@ RSpec.describe "Users", type: :request do
   describe 'DELETE /users/1' do
     let(:path) { '/users/1' }
 
+    context 'When legal token is not contained in header' do
+      it 'Response with 401' do
+        delete path
+        expect(response).to have_http_status(401)
+      end
+    end
+    
     context 'user exist' do
       it 'Response with 200 and user deleted' do
         Data.users[:data] << {
@@ -170,7 +198,7 @@ RSpec.describe "Users", type: :request do
           email: 'victim@mail.com'
         }
 
-        delete path
+        delete path, headers: header_with_token
         expect(response).to have_http_status(200)
         expect(Data.users[:data]).to eq([])
       end
@@ -178,7 +206,7 @@ RSpec.describe "Users", type: :request do
 
     context 'user not exist' do
       it 'Response with 400' do
-        delete path
+        delete path, headers: header_with_token
         expect(response).to have_http_status(400)
       end
     end
